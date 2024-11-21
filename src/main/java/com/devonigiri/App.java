a342cc5baa2c087fc01974fb988d83fb47335fda
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2024-11-21 12:41:14 -0300

    initial commit

---
+++ src/main/java/com/devonigiri/App.java
@@ -0,0 +1,15 @@
+package com.devonigiri;
+
+import com.github.cliftonlabs.json_simple.JsonObject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(" Hello World ");
+    }
+}
