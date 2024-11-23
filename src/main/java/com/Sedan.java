745023f561c12b2ebc2aefc67a61049aa3100770
anabeatriz000 <anabeatriz.vanderlei@upe.br>
2024-11-22 21:52:20 -0300

    Modifiquei algimas classes

---
+++ src/main/java/com/Sedan.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Sedan extends Veiculo {
+    public Sedan(String marca, String modelo, double tarifaDiaria) {
+        super(marca, modelo, tarifaDiaria);
+    }
+
+    @Override
+    public void exibirDetalhes() {
+        super.exibirDetalhes();
+        System.out.println("Tipo: Sedan");
+    }
+    
+}
