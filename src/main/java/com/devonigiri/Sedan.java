78aa4227186bb88e194e838b0b548a4eebd4fb61
anabeatriz000 <anabeatriz.vanderlei@upe.br>
2024-12-02 14:03:34 -0300

    Update Sedan.java

--- src/main/java/com/devonigiri/Sedan.java
+++ src/main/java/com/devonigiri/Sedan.java
@@ -1,14 +1,26 @@
 package com.devonigiri;
 
 public class Sedan extends Veiculo {
-    public Sedan(String marca, String modelo, double tarifaDiaria) {
+    private String arCondicionado;
+    public Sedan(String marca, String modelo, double tarifaDiaria, String arCondicionado) {
         super(marca, modelo, tarifaDiaria);
+        this.arCondicionado = arCondicionado;
     }
+    public String getArCondicionado() {
+        return arCondicionado;
+    }
+
+    public void setArcondicionado(String arCondicionado) {
+        this.arCondicionado = arCondicionado;
+    }
+    
+    
 
     @Override
     public void exibirDetalhes() {
         super.exibirDetalhes();
-        System.out.println("Tipo: Sedan");
+        System.out.println("AR Condicionado: " + getArCondicionado());
+        System.out.println("Tipo: Sedan" );
     }
 
 }
