2690ad1b627559a79b7db7cb8ae43b04085a6bf0
anabeatriz000 <anabeatriz.vanderlei@upe.br>
2024-12-02 14:03:50 -0300

    Update Suv.java

--- src/main/java/com/devonigiri/Suv.java
+++ src/main/java/com/devonigiri/Suv.java
@@ -1,14 +1,25 @@
 package com.devonigiri;
 
 public class Suv extends Veiculo {
-    public Suv(String marca, String modelo, double tarifaDiaria) {
+    private String direcaoAutomatica; 
+    public Suv(String marca, String modelo, double tarifaDiaria, String direcaoAutomatica) {
         super(marca, modelo, tarifaDiaria);
+        this.direcaoAutomatica = direcaoAutomatica;
     }
+    public String getDirecaoAutomatica() {
+        return direcaoAutomatica;
+    }
+
+    public void setDirecaoAutomatica(String direcaoAutomatica) {
+        this.direcaoAutomatica = direcaoAutomatica;
+    }
+
 
 
     @Override
     public void exibirDetalhes() {
         super.exibirDetalhes();
+        System.out.println("Direção Automática: " + getDiasReservados());
         System.out.println("Tipo: SUV");
     }
     
