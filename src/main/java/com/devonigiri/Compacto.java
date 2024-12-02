d0eeffbd0fbc2eb2164b6e961d535e6c846a26df
anabeatriz000 <anabeatriz.vanderlei@upe.br>
2024-12-02 14:03:18 -0300

    Update Compacto.java

--- src/main/java/com/devonigiri/Compacto.java
+++ src/main/java/com/devonigiri/Compacto.java
@@ -1,13 +1,22 @@
 package com.devonigiri;
 
 public class Compacto extends Veiculo {
-    public Compacto(String marca, String modelo, double tarifaDiaria) {
+    private String gps;
+    public Compacto(String marca, String modelo, double tarifaDiaria, String gps) {
         super(marca, modelo, tarifaDiaria);
+        this.gps = gps;
+    }
+    public String getGps() {
+        return gps;
     }
 
+    public void setGps(String gps) {
+        this.gps = gps;
+    }
     @Override
     public void exibirDetalhes() {
         super.exibirDetalhes();
+        System.out.println("GPS: " + getGps());
         System.out.println("Tipo: Compacto");
     }
     
